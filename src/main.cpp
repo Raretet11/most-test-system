695eedb93fd9c45594c6c47f2c58b7264909a9ee
Raretet11 <MagicRaretet@gmail.com>
2025-02-16 22:43:47 +0300

    remove example 'hello' API

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
+#include <userver/clients/dns/component.hpp>
 #include <userver/clients/http/component.hpp>
+#include <userver/components/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 #include "admin_component/load_task.hpp"
-#include "hello.hpp"
 #include "user_component/submit_solution.hpp"
 
 int main(int argc, char *argv[]) {
@@ -14,10 +16,11 @@ int main(int argc, char *argv[]) {
             .Append<userver::server::handlers::Ping>()
             .Append<userver::components::TestsuiteSupport>()
             .Append<userver::components::HttpClient>()
-            .Append<userver::server::handlers::TestsControl>();
+            .Append<userver::server::handlers::TestsControl>()
+            .Append<userver::components::Postgres>("postgres-db-1")
+            .Append<userver::clients::dns::Component>();
 
     most::append_task_loader_component(component_list);
-    most::AppendHello(component_list);
     most::append_task_submiter_component(component_list);
 
     return userver::utils::DaemonMain(argc, argv, component_list);
