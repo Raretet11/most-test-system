786bd4738b21d66f58a89eda418aef9f8719bce5
Raretet11 <MagicRaretet@gmail.com>
2025-03-01 12:31:09 +0300

    fix line len in tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,7 +8,8 @@ from testsuite.databases import pgsql
 
 async def test_load_task(service_client):
     response = await service_client.post(
-        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1%202%233%7C3%204%237%7C5%206%2311',
+        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1%202%\
+            233%7C3%204%237%7C5%206%2311',
     )
     assert response.status == 200
     assert response.text == 'OK'
@@ -16,13 +17,16 @@ async def test_load_task(service_client):
 
 async def test_load_task_and_send_ok_solution(service_client):
     response = await service_client.post(
-        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1%202%233%7C3%204%237%7C5%206%2311',
+        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1\
+        %202%233%7C3%204%237%7C5%206%2311',
     )
     assert response.status == 200
     assert response.text == 'OK'
     
     response = await service_client.post(
-        '/v1/submit?task-id=1&code=import%20sys%0Ainput_data%20%3D%20sys.argv%5B1%5D%0Aa%2C%20b%20%3D%20map(int%2C%20input_data.split())%0Aprint(a%20%2B%20b)',
+        '/v1/submit?task-id=1&code=import%20sys%0Ainput_data\
+            %20%3D%20sys.argv%5B1%5D%0Aa%2C%20b%20%3D%20map\
+                (int%2C%20input_data.split())%0Aprint(a%20%2B%20b)',
     )
     assert response.status == 200
     assert response.text == 'OK'
@@ -30,13 +34,16 @@ async def test_load_task_and_send_ok_solution(service_client):
 
 async def test_load_task_and_send_wa_solution(service_client):
     response = await service_client.post(
-        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1%202%233%7C3%204%237%7C5%206%2311',
+        '/v1/load?name=Sum%20of%20Two%20Numbers&tests=1\
+            %202%233%7C3%204%237%7C5%206%2311',
     )
     assert response.status == 200
     assert response.text == 'OK'
     
     response = await service_client.post(
-        'v1/submit?task-id=1&code=import%20sys%0Ainput_data%20%3D%20sys.argv%5B1%5D%0Aa%2C%20b%20%3D%20map(int%2C%20input_data.split())%0Aprint(a%20%2B%20b+1)',
+        'v1/submit?task-id=1&code=import%20sys%0Ainput_data%20%3D%20sys.\
+            argv%5B1%5D%0Aa%2C%20b%20%3D%20map(int%2C%20input_data.split())\
+                %0Aprint(a%20%2B%20b+1)',
     )
     assert response.status == 200
     assert response.text == 'WRONG_ANSWER'
