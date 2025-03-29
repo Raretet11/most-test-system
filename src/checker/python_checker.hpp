55026096e33b6bb5f6c54238d3ff7773a36389ef
Raretet11 <MagicRaretet@gmail.com>
2025-03-29 18:39:39 +0300

    MOSTTETSSYS-12: fix multi-solution testing

--- src/checker/python_checker.hpp
+++ src/checker/python_checker.hpp
@@ -7,18 +7,24 @@ namespace checker {
 
 class PythonChecker : public AbstractChecker {
 public:
+    PythonChecker();
+
     [[nodiscard]] std::vector<SubmissionFeedback> check_solution(
         const std::string &solution,
         const std::vector<Problem> &problems
     ) const override;
 
 private:
+    inline static std::size_t _global_checker_index = 0;
+    std::size_t _checker_index;
+
     std::string get_code_file_name() const;
     std::string get_checker_name() const;
     std::string get_output_file_name() const;
     std::string get_error_file_name() const;
 
     void generate_files(const std::string &solution) const;
+    void delete_files() const;
     SubmissionFeedback check_test(const Problem &problem) const override;
 };
 
